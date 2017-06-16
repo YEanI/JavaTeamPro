22e3e0786594de8d977bb9e223fcb233a6ef061b
YEanI <yeani97@naver.com>
2017-06-16 16:29:34 +0900

    폭탄 이미지를 알파벳으로 바꾸고 알파벳마다 가속도, 나오는 확률을 다르게 함

--- src/data/Bomb.java
+++ src/data/Bomb.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import java.awt.*;
 
 import static app.GameConstants.DEFAULT_BOMB_SIZE;
+import static data.Bomb.Grade.*;
 
 /**
  * Created by 이예은 on 2017-06-08.
@@ -14,13 +15,11 @@ import static app.GameConstants.DEFAULT_BOMB_SIZE;
 public class Bomb {
     private DrawingObject object;
     private double dy, ay;
+    private Grade grade;
 
     public Bomb() {
-        object = new DrawingObject();
-        object.setImage("/images/ic_add_circle_outline_black.png", DEFAULT_BOMB_SIZE);
         dy = 0;
         ay = 0;
-
     }
 
     public void move() {
@@ -29,5 +28,14 @@ public class Bomb {
         point.setLocation(point.getX(), point.getY() + dy);
     }
 
+    public void setImage(String s, int defaultBombSize) {
+        object = new DrawingObject();
+        object.setImage(s, defaultBombSize);
+    }
+
+    public enum Grade {
+        A, B, C, D, F;
+
+    }
 
 }
